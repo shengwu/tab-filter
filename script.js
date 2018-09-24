afef613cbc962ec75571ae2bc85e2746d2670b67
Sheng Wu <shengwu34@gmail.com>
2018-09-24 09:48:25 -0700

    WIP

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function LastUpdated(last_updated) {
   return 'Last updated ' + date + '.';
 }
 
-function Tab(props) {
+function Tab(id, props) {
   var downloads = [];
   for (var type in props.download) {
     if (props.download.hasOwnProperty(type)) {
@@ -25,23 +25,22 @@ function Tab(props) {
   var downloadElem =
     '<p><strong>Downloads: </strong>' + downloads.join(', ') + '</p>';
 
-  var tabElem = '<div class="tab">' + 
-    '<h3>' + props.title + '</h3>' +
+  var tab = document.createElement('div');
+  tab.classList = 'tab';
+  tab.setAttribute('data-id', id);
+  var internals = '<h3>' + props.title + '</h3>' +
     downloadElem +
     '<p>' + LastUpdated(props.last_updated) + ' ' +
       PostedBy(props.posted_by) + '</p>';
   if (props.desc) {
-    tabElem += '<p>' + props.desc + '</p>';
+    internals += '<p>' + props.desc + '</p>';
   }
-  tabElem += '</div>';
-  return tabElem;
+  tab.innerHTML = internals;
+  return tab;
 }
 
-function filterTabs(filter) {
-  return function(tab) {
-    return tab.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
-  }
-}
+
+
 
 var debounceMs = 200;
 var doFilterId = -1;
@@ -51,14 +50,96 @@ function onFilterChange(e) {
   doFilterId = setTimeout(render, debounceMs);
 }
 
+function filterTabs(filter) {
+  return function(tab) {
+    return tab.title.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
+  }
+}
+
+
+
+var tabCache = {};
+
+function preRenderTabs() {
+  for (var i = 0; i < TABS.length; i++) {
+    tabCache[i] = Tab(i, TABS[i]);
+    TABS[i].id = i;
+  }
+}
+
+function getTabId(tab) {
+  return tab.id;
+}
+
+function getTabsToAdd(old, next) {
+  var tabsToAdd = [];
+  for (id in next) {
+    if (!old[id]) {
+      tabsToAdd.push(id);
+    }
+  }
+  tabsToAdd.sort(function(a, b) { return a - b; });
+  return tabsToAdd.map(function(str) { return parseInt(str); });
+}
+
+function getTabsToDelete(old, next) {
+  var tabsToDelete = {};
+  for (id in old) {
+    if (!next[id]) {
+      tabsToDelete[id] = true;
+    }
+  }
+  return tabsToDelete;
+}
+
+// TODO: there's a bug somewhere!
+function updateTabs(nextTabs) {
+  var tabContainer = document.getElementById('tabs');
+  var tabObjs = tabContainer.children;
+  var it = 0;
+  for (var i = 0; i < tabObjs.length; i++) {
+    var currTab = tabObjs[i];
+    var currId = parseInt(currTab.getAttribute('data-id'));
+    while (it < currId) {
+      if (nextTabs[it]) {
+        tabContainer.insertBefore(tabCache[it], currTab);
+      }
+      it += 1;
+    }
+    console.log(currTab);
+    if (!nextTabs[currId]) {
+      tabContainer.removeChild(currTab);
+      i -= 1;
+    }
+  }
+
+  while (it < TABS.length) {
+    if (nextTabs[it]) {
+      // passing in null appends the new element at the end
+      tabContainer.insertBefore(tabCache[it], null);
+    }
+    it += 1;
+  }
+}
+
+function listToDict(list) {
+  var dict = {};
+  for (var i = 0; i < list.length; i++) {
+    dict[i] = true;
+  }
+  return dict;
+}
+
 function render() {
   var filterVal = document.getElementById('tab-filter').value.trim();
   if (filterVal !== '') {
-    var tabs = TABS.filter(filterTabs(filterVal)).map(Tab).join('');
+    var nextTabs = TABS.filter(filterTabs(filterVal)).map(getTabId);
   } else {
-    var tabs = TABS.map(Tab).join('');
+    var nextTabs = TABS.map(getTabId);
   }
-  document.getElementById('tabs').innerHTML = tabs;
+  console.log(nextTabs);
+  updateTabs(listToDict(nextTabs));
 }
 
+preRenderTabs();
 render();
